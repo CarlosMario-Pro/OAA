bdf0f8958d5d68e228c195c8945c18af14d0ff2b
MarioReyes <cmario.reyesp@gmail.com>
2023-04-05 23:31:42 -0500

    Refactorización rutas para News

---
+++ Frontend/src/Pages/Detail/Detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Detail.module.css';
+
+
+export default function Detail () {
+    
+  return (
+    <div>
+        <h1>Detail</h1>
+    </div>
+  );
+};
