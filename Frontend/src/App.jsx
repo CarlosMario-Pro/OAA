8ad4f98bc1aecad7b7aac32bca341ed0fbb4c034
Dariana Rengifo <darianarengifo@gmail.com>
2023-04-12 09:36:42 -0300

    agregar rutas de la app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import axios from "axios";
 import { Navigate, Route, Routes } from "react-router-dom";
+import AlertWindow from "./Components/Alerts/AlertWindow";
 import LandingPage from "./Pages/LadingPage/LandingPage";
 import Detail from "./Pages/Detail/Detail";
 import Error404 from "./Pages/Error404/Error404";
+import Loader from "./Components/Loader/Loader";
 import LoginAdmin from "./Pages/LoginAdmin/LoginAdmin";
 import PanelAdmin from "./Pages/PanelAdmin/PanelAdmin";
-import GalleryImageForm from "./Components/PanelAdminForms/GalleryImageForm";
-import GalleryVideoForm from "./Components/PanelAdminForms/GalleryVideoForm";
-import OurProjectsForm from "./Components/PanelAdminForms/OurProjectsForm";
-import PublicationsForm from "./Components/PanelAdminForms/PublicationsForm";
-import RadioProgramForm from "./Components/PanelAdminForms/RadioProgramForm";
-import Loader from "./Components/Loader/Loader";
-import AlertWindow from "./Components/Alerts/AlertWindow";
+import PanelAdminForm from "./Pages/PanelAdminForm/PanelAdminForm";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
@@ -28,11 +24,7 @@ function App() {
         <Route path='/login' element={<LoginAdmin />} />
         <Route element={<ProtectedRoute />}>
           <Route path='/panel-admin' element={<PanelAdmin />} />
-          <Route path='/gallery-image-form' element={<GalleryImageForm />} />
-          <Route path='/gallery-video-form' element={<GalleryVideoForm />} />
-          <Route path='/our-projects-form' element={<OurProjectsForm />} />
-          <Route path='/publications-form' element={<PublicationsForm />} />
-          <Route path='/radio-program-form' element={<RadioProgramForm />} />
+          <Route path='/panel-admin/:form' element={<PanelAdminForm />} />
         </Route>
         <Route path='*' element={<Error404 />} />
       </Routes>
