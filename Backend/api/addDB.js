be4e8ec8ff4102100c069ecae5af034f872bdd9d
Dariana Rengifo <darianarengifo@gmail.com>
2023-04-30 11:13:24 -0300

    Update addDB.js

--- Backend/api/addDB.js
+++ Backend/api/addDB.js
@@ -1,15 +1,19 @@
 const News = require("./models/News");
-const NewsDB = require("./additional/NewsDB");
+const news = require("./additional/NewsDB");
+const Galleries = require("./models/Galleries");
+const galleries = require("./additional/GalleryDB");
+const OurWorks = require("./models/OurWorks");
+const works = require("./additional/OurWorkDB");
 
 async function loadedNews() {
   try {
     const count = await News.countDocuments(); //Obtener la cantidad de documentos en la colección 'Products' de la DB.
     if (count > 0) {
       //Si hay algún documento presente, la función simplemente imprime "Productos cargados anteriormente" y finaliza.
-      console.log("News cargadas anteriormente");
+      console.log("Noticias cargadas anteriormente");
       return;
     }
-    await News.insertMany(NewsDB); //Se inserta todos los documentos de productos (contenidos en el array products) en la colección 'Products' usando Products.insertMany(products).
+    await News.insertMany(news); //Se inserta todos los documentos de productos (contenidos en el array products) en la colección 'Products' usando Products.insertMany(products).
     console.log("NewsDB en Mongo Atlas"); //El resultado de esta consulta se imprime por consola
   } catch (err) {
     console.log(err);
@@ -17,4 +21,36 @@ async function loadedNews() {
   }
 }
 
-module.exports = { loadedNews };
+async function loadedGalleries() {
+  try {
+    const count = await Galleries.countDocuments(); //Obtener la cantidad de documentos en la colección 'Products' de la DB.
+    if (count > 0) {
+      //Si hay algún documento presente, la función simplemente imprime "Productos cargados anteriormente" y finaliza.
+      console.log("Galería cargada anteriormente");
+      return;
+    }
+    await Galleries.insertMany(galleries); //Se inserta todos los documentos de productos (contenidos en el array products) en la colección 'Products' usando Products.insertMany(products).
+    console.log("GalleryDB en Mongo Atlas"); //El resultado de esta consulta se imprime por consola
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+async function loadedWorks() {
+  try {
+    const count = await OurWorks.countDocuments(); //Obtener la cantidad de documentos en la colección 'Products' de la DB.
+    if (count > 0) {
+      //Si hay algún documento presente, la función simplemente imprime "Productos cargados anteriormente" y finaliza.
+      console.log("sección 'Nuestro trabajo' cargado anteriormente");
+      return;
+    }
+    await OurWorks.insertMany(works); //Se inserta todos los documentos de productos (contenidos en el array products) en la colección 'Products' usando Products.insertMany(products).
+    console.log("WorksDB en Mongo Atlas"); //El resultado de esta consulta se imprime por consola
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+module.exports = { loadedNews, loadedGalleries, loadedWorks };
