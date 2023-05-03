368e260aaab66f73175f7698e61488323b4bf969
MarioReyes <cmario.reyesp@gmail.com>
2023-05-02 23:49:25 -0500

    hotfix: Se corrige la eliminacion del archivo email.utils.js

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const app = require("./app.js");
 const { connection } = require("./db.js");
 const { loadedNews, loadedGalleries, loadedWorks } = require("./addDB");
+const { transport } = require("./utils/email.utils.js");
 const { PORT } = process.env;
 
 connection
@@ -11,6 +12,8 @@ connection
     await loadedNews();
     await loadedGalleries();
     await loadedWorks();
+    await transport.verify();
+    console.log("nodemailer conectado exitosamente.");
   })
   .then(() => {
     app.listen(PORT, () => {
